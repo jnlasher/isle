3690d60ac152974069f377b7bcf4e45dde99336c
jnlasher <jnlasher@gmail.com>
2017-02-14 12:34:30 -0500

    v0.2
    Added commands to update SQL and config files.

--- new-instance.sh
+++ new-instance.sh
@@ -7,6 +7,10 @@ read new_instance
 cp -r /var/www/instances/myinstance/. /var/www/instances/$new_instance  # Copy original instance to new directory
 find /var/www/instances/$new_instance -type f -name "*.log" -delete	# Delete all log files
 
+# Update the data.sql and init.sql files with the new instance name
+sed -i'' -e 's/\myinstance/'$new_instance'/g' /var/www/instances/$new_instance/data.sql
+sed -i'' -e 's/\myinstance/'$new_instance'/g' /var/www/instances/$new_instance/init.sql
+
 # Find all file names that contain myinstance and replace them with the new instance name
 find /var/www/instances/$new_instance -type f -name "*myinstance*" | while read F; do 
 	nf="$(echo ${F} | sed -e 's/\myinstance/'$new_instance'/')" ; 
@@ -24,3 +28,6 @@ sed -i "/Initialize db tables/ a mysql -uroot -p'root' -h localhost isle_dev < \
 sed -i "/Create apache conf/ a cp /var/www/instances/$new_instance/isle.local.$new_instance.conf /etc/apache2/sites-available/isle.local.$new_instance.conf\na2ensite isle.local.$new_instance" /var/www/isle-init.sh
 
 sed -i "/Create logrotate conf/ a cat <<EOT >> /etc/logrotate.d/isle-foobar\n/var/www/instances/$new_instance/logs/*.log {\n\tyearly\n\tmaxsize 2M\n\trotate 5\n\tnotifempty\n\tmissingok\n\tsu vagrant vagrant\n}\nEOT" /var/www/isle-init.sh
+
+
+sed -i'' -e 's/\myinstance/'$new_instance'/g' /var/www/instances/$new_instance/isle.local.$new_instance.conf
